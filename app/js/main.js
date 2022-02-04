b5eaedaa7f28448739ae644c854006047682f6b5
ReinmandTrial <max.gluschtsov@gmail.com>
2022-02-05 02:29:28 +0300

    layout 3 block responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,3 @@
- 
-  new Swiper('.certificate__swiper', {
-    direction: 'horizontal',
-	navigation: {
-		prevEl: '.btn__swiper-prev',
-		nextEl: '.btn__swiper-next',
-	},
-    autoHeight: true,
-	slidesPerView: 1,
-})
-
 new Swiper('.reviews__swiper', {
 	direction: 'horizontal',
 	navigation: {
@@ -19,10 +8,35 @@ new Swiper('.reviews__swiper', {
 		el: '.swiper-pagination',
 		type: 'bullets',
 	},
+	breakpoints: {
+		1163: {
+			spaceBetween: 55,
+			slidesPerView: 3,
+		},
+		942: {
+			slidesPerView: 3,
+		},
+		769: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+			// slidesPerView: 2,
+		},
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		},
+		// 576: {
+		// 	slidesPerView: 'auto',
+		// },
+		// 320: {
+		// 	slidesPerView: 2,
+		// 	spaceBetween: 8,
+		// }
+	},
 	// loop: true,
     autoHeight: true,
-	slidesPerView: 3,
-	spaceBetween: 55,
+	// slidesPerView: 3,
+	// spaceBetween: 55,
 	// initialSlide: 1,
 	watchOverflow: true,
 	mousewheelControl: false,
@@ -34,11 +48,30 @@ new Swiper('.partners__swiper', {
 		prevEl: '.btn__swiper-prev',
 		nextEl: '.btn__swiper-next',
 	},
+	breakpoints: {
+		992: {
+			spaceBetween: 26,
+			slidesPerView: 3,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+	},
 	// loop: true,
     autoHeight: true,
-	slidesPerView: 3,
-	spaceBetween: 26,
 	// initialSlide: 1,
 	watchOverflow: true,
 	mousewheelControl: false,
+})
+
+
+new Swiper('.certificate__swiper', {
+    direction: 'horizontal',
+	navigation: {
+		prevEl: '.btn__swiper-prev',
+		nextEl: '.btn__swiper-next',
+	},
+    autoHeight: true,
+	slidesPerView: 1,
 })
