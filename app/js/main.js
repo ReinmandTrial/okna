0f392f7481ab93d8f8649cab4673b13679a1ea11
ReinmandTrial <max.gluschtsov@gmail.com>
2022-02-04 15:40:23 +0300

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+ 
+  new Swiper('.certificate__swiper', {
+    direction: 'horizontal',
+    // loop: true,
+	navigation: {
+		prevEl: '.btn__swiper-prev',
+		nextEl: '.btn__swiper-next',
+	},
+	// mousewheel: {
+	// 	sensitivity: 1,
+	// },
+	// breakpoints: {
+	// 	769: {
+	// 		spaceBetween: 20,
+	// 	},
+	// 	576: {
+	// 		slidesPerView: 'auto',
+	// 	},
+	// 	320: {
+	// 		slidesPerView: 2,
+	// 		spaceBetween: 8,
+	// 	}
+	// },
+    autoHeight: true,
+	slidesPerView: '1',
+    slidesPerColumn: '1',
+	// watchOverfбез low: true,
+	// slidesPerView: 1,
+	// watchOverflow: true,
+	// spaceBetween: 20,
+	// loop: true,
+	// mousewheelControl: false,
+})
